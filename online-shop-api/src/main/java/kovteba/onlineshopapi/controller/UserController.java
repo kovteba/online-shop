60d61c771e5f2e2ae105fd5afc5478d86b5940e0
Kovteba Dima <kovteba@gmail.com>
2020-04-27 18:04:49 +0300

    Add controller for show pdf

--- online-shop-api/src/main/java/kovteba/onlineshopapi/controller/UserController.java
+++ online-shop-api/src/main/java/kovteba/onlineshopapi/controller/UserController.java
@@ -1,10 +1,6 @@
 package kovteba.onlineshopapi.controller;
 
 import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfWriter;
-import io.swagger.annotations.SwaggerDefinition;
 import kovteba.onlineshopapi.entity.UserEntity;
 import kovteba.onlineshopapi.mapper.UserMapper;
 import kovteba.onlineshopapi.responce.Responce;
@@ -14,42 +10,30 @@ import kovteba.onlineshopapi.util.GeneratePDF;
 import kovteba.onlineshopapi.util.JwtTokenUtil;
 import kovteba.onlineshopcommon.enums.RoleUser;
 import kovteba.onlineshopcommon.pojo.User;
-import lombok.AllArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.codehaus.commons.compiler.util.resource.FileResource;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.*;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
-
 @RestController
 @RequestMapping("/user")
-@AllArgsConstructor
 public class UserController {
 
-//    @Value("${online.out.storage.pdf}")
-//    private String directoty;
-
-    //    private final String directoty = "online-shop-api/src/main/resources/pdf/";//files/
-    private final String directoty = "files/";//files/
+    @Value("${online.out.storage.pdf}")
+    private String directory;
 
     private final Logger log = LoggerFactory.getLogger(UserController.class);
 
@@ -62,7 +46,18 @@ public class UserController {
     private final EmailService emailService;
 
     private final UserMapper userMapper;
-    private final ResourceLoader resourceLoader;
+
+    public UserController(UserService userService,
+                          JwtTokenUtil jwtTokenUtil,
+                          GeneratePDF generatePDF,
+                          EmailService emailService,
+                          UserMapper userMapper) {
+        this.userService = userService;
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.generatePDF = generatePDF;
+        this.emailService = emailService;
+        this.userMapper = userMapper;
+    }
 
     @GetMapping("/role")
     public ResponseEntity<User> getUserByRole(@RequestHeader(value = "Authorization") String token,
@@ -89,8 +84,7 @@ public class UserController {
         String email = jwtTokenUtil.getEmailFromToken(token);
         Responce responce = userService.getUserByEmail(email);
         User user = userMapper.userEntityToUser((UserEntity) responce.getObject());
-        return ResponseEntity.status(responce.getStatus())
-                .body(user);
+        return ResponseEntity.status(responce.getStatus()).body(user);
     }
 
     @PostMapping("/basket/{userId}/{productId}")
@@ -104,54 +98,64 @@ public class UserController {
                 .body(userMapper.userEntityToUser((UserEntity) responce.getObject()));
     }
 
-
-    private static final String INTERNAL_FILE = "irregular-verbs-list.pdf";
-    private static final String EXTERNAL_FILE_PATH = "files/user1gmail.comReceipt.zip";
-
-
-    @GetMapping("/genRec/{userId}")
-    public void  /*ResponseEntity<ByteArrayResource>*/ generateReceipt(@RequestHeader(value = "Authorization") String token,
-                                                                       @PathVariable Long userId,
-                                                                       HttpServletResponse response,
-                                                                       HttpServletRequest request) throws IOException, DocumentException {
-
-        UserEntity userEntity = (UserEntity) userService.getUserById(userId).getObject();
-        String fileName = generatePDF.generateDPF(userEntity.getBasket(), userEntity.getEmail());
-
-//        Path path = Paths.get(directoty + fileName);
-//        byte[] data = Files.readAllBytes(path);
-//        ByteArrayResource resource = new ByteArrayResource(data);
-//        return ResponseEntity.ok()
-//                // Content-Disposition
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + path.getFileName().toString())
-//                // Content-Type
-//                .contentType(MediaType.APPLICATION_PDF)
-//                // Content-Lengh
-//                .contentLength(data.length)
-//                .body(resource);
-
-
-        File file = new File(directoty + fileName);
-        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
-        response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
-        response.setContentLength((int) file.length());
-        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-        FileCopyUtils.copy(inputStream, response.getOutputStream());
-
-
-
-
-//        byte[] contents = document.toString().getBytes();
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_PDF);
-//        headers.setContentDispositionFormData("online-shop-api/src/main/resources/pdf/user1Receipt.pdf", "online-shop-api/src/main/resources/pdf/user1Receipt.pdf");
-//        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-//        return new ResponseEntity<>(contents, headers, HttpStatus.OK);
+    @GetMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(@RequestHeader(value = "Authorization") String token) {
+        emailService.sendSimpleMessage("kovteba@gmail.com", "Test", "TEST TEXT");
+        return ResponseEntity.status(HttpStatus.CREATED).body("CREATE SECRET TOKEN");
     }
 
-    @GetMapping("/sendEmail")
-    public void sendEmail(@RequestHeader(value = "Authorization") String token) {
-        emailService.sendSimpleMessage("kovteba@gmail.com", "Test", "TEST TEXT");
+    @GetMapping(
+            value = "/genRec/{userId}",
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public void generateReceipt(@RequestHeader(value = "Authorization") String token,
+                                @PathVariable Long userId,
+                                HttpServletResponse response) throws IOException {
+        UserEntity userEntity = (UserEntity) userService.getUserById(userId).getObject();
+        String fileName = generatePDF.generateDPF(userEntity.getBasket(), userEntity.getEmail());
+        File file = new File(directory + fileName);
+        InputStream targetStream = new DataInputStream(new FileInputStream(file));
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        IOUtils.copy(targetStream, response.getOutputStream());
+    }
+
+
+//    //////////////////////////////////////////////////////////////////////////
+    @GetMapping(
+            value = "/pic",
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public void getPic(HttpServletResponse response) throws IOException, DocumentException {
+        File file = new File(directory + "IMG_1050.JPEG");
+        InputStream targetStream = new DataInputStream(new FileInputStream(file));
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        IOUtils.copy(targetStream, response.getOutputStream());
+    }
+
+    @GetMapping(
+            value = "/download",
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<ByteArrayResource> downloadByClick() throws IOException {
+        UserEntity userEntity = (UserEntity) userService.getUserById(1L).getObject();
+        String fileName = generatePDF.generateDPF(userEntity.getBasket(), userEntity.getEmail());
+        Path path = Paths.get(directory + fileName);
+        byte[] data = Files.readAllBytes(path);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + path.getFileName().toString())
+                .contentType(MediaType.APPLICATION_PDF)
+                .contentLength(data.length)
+                .body(resource);
+    }
+
+    @GetMapping(
+            value = "/show",
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public void showByClick(HttpServletResponse response) throws IOException, DocumentException {
+        UserEntity userEntity = (UserEntity) userService.getUserById(1L).getObject();
+        String fileName = generatePDF.generateDPF(userEntity.getBasket(), userEntity.getEmail());
+        File file = new File(directory + fileName);
+        InputStream targetStream = new DataInputStream(new FileInputStream(file));
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        IOUtils.copy(targetStream, response.getOutputStream());
     }
 
 }
