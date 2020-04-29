a95d668be0d1bceffcec65bbb84ec0faeebe61ac
Kovteba Dima <kovteba@gmail.com>
2020-04-29 15:55:10 +0300

    Clean Application

--- online-shop-api/src/main/java/kovteba/onlineshopapi/OnlineShopApiApplication.java
+++ online-shop-api/src/main/java/kovteba/onlineshopapi/OnlineShopApiApplication.java
@@ -1,11 +1,7 @@
 package kovteba.onlineshopapi;
 
-import kovteba.onlineshopapi.mapper.UserMapper;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
