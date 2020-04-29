b25a32202210400e81279e875bfe8d503f9ed8c9
Kovteba Dima <kovteba@gmail.com>
2020-04-29 15:53:47 +0300

    Change retirn type to List<User>

--- online-shop-api/src/main/java/kovteba/onlineshopapi/repository/UserRepository.java
+++ online-shop-api/src/main/java/kovteba/onlineshopapi/repository/UserRepository.java
@@ -5,11 +5,13 @@ import kovteba.onlineshopapi.entity.UserEntity;
 import kovteba.onlineshopcommon.enums.RoleUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     UserEntity findByPhoneNumber(String phoneNumber);
 
-    UserEntity findByRoleUser(RoleUser roleUser);
+    List<UserEntity> findByRoleUser(RoleUser roleUser);
 
     UserEntity findByEmail(String email);
 
